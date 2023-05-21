c9e1e894694570a30c3a0d9b9671020422d2229e
mrkhoshalhan <mrkhoshalhan1399@gmail.com>
2023-05-21 18:32:28 +0430

    rubika bot

---
+++ README.md
@@ -0,0 +1,6 @@
+#bot rubika
+
+auther: farhad khoshalhan
+Description: This project is for sending messages in Rubika's inbox and the ability to delete and edit the sent text. I hope friends can use it well.
+ 
+ In this project, we used the Rubik's library prepared by Bahman Ahmadi!
---
+++ app.py
@@ -0,0 +1,13 @@
+from rubika.client import Bot
+
+
+
+# acc token
+bot= Bot(  appName= "appName" , auth="rwoglhomlbgyjszisaniqpxvwicrgsnq")
+#acc typ
+guid ="c0B6lL6088f56807485e2efb50f4a466"
+bot.sendMessage(guid,"hi")
+
+# print(bot.getInfo)
+# bot.editMessage(12333344,guid,"1234567890")
+# bot.deleteMessages(guid,["12333344"])
